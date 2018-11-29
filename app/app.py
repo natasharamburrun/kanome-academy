ce4fb76231e79a8a6dbc527c586135a7cd53f198
natasharamburrun <r_natasha100@hotmail.com>
2018-11-29 10:39:43 +0000

    added the url to the route

--- app/app.py
+++ app/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, jsonify
 
+import requests
+
 from .config import get_client
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
+@app.route('/', methods=['GET'])
+def boards():
     cilent = get_client()
+    endpoint = 'https://api.trello.com/1/boards'
+    response = requests.request("GET", url)
+    return jsonify
 
-    return 'Hello, World!'
+    # return 'Hello, World!'
 
 @app.route('/index')
 def index():

ac8186b9cd652e732770b0901fb96540b1b5b160
natasharamburrun <r_natasha100@hotmail.com>
2018-11-30 11:30:25 +0000

    added a dump of the api data

--- app/app.py
+++ app/app.py
@@ -14,17 +14,14 @@ def boards():
     client = get_client()
     API_KEY = os.getenv("API_KEY")
     TOKEN = os.getenv("TOKEN")
-    url = 'https://api.trello.com/1/boards/5bb34b7b941a295e2ecd1c12?key={api_key}&token={token}'.format(api_key=API_KEY, token=TOKEN)
+    url = 'https://api.trello.com/1/boards/5bb34b7b941a295e2ecd1c12/cards?key={api_key}&token={token}'.format(api_key=API_KEY, token=TOKEN)
     response = requests.request("GET", url)
-    # print(response.text)
-    # print(response.json())
-
+    print(response.json())
     res = response.json()
 
-    return jsonify(id=res['id'], name=res['name'])
-    # return 'done'
-
-    # return 'Hello, World!'
+    # print(res[0]['id'])
+    # return jsonify(id=res[0]['id'])
+    return 'done'
 
 @app.route('/index')
 def index():

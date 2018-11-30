85a9fc792f0448040a1310bd4a3e92317244a1ec
natasharamburrun <r_natasha100@hotmail.com>
2018-11-30 10:07:57 +0000

    API data available

--- app/app.py
+++ app/app.py
@@ -13,10 +13,16 @@ app = Flask(__name__)
 def boards():
     client = get_client()
     API_KEY = os.getenv("API_KEY")
-    OAUTH_TOKEN = os.getenv("API_SECRET")
-    url = 'https://api.trello.com/1/boards/5bb34b7b941a295e2ecd1c12?key={api_key}&token={oauth_token}'.format(api_key=API_KEY, oauth_token=OAUTH_TOKEN)
-    print(response.text)
-    return jsonify
+    TOKEN = os.getenv("TOKEN")
+    url = 'https://api.trello.com/1/boards/5bb34b7b941a295e2ecd1c12?key={api_key}&token={token}'.format(api_key=API_KEY, token=TOKEN)
+    response = requests.request("GET", url)
+    # print(response.text)
+    # print(response.json())
+
+    res = response.json()
+
+    return jsonify(id=res['id'], name=res['name'])
+    # return 'done'
 
     # return 'Hello, World!'
 
--- app/config.py
+++ app/config.py
@@ -7,14 +7,14 @@ def get_client():
     load_dotenv()
     api_key=os.getenv("API_KEY")
     api_secret=os.getenv("API_SECRET")
-    oauth_token=os.getenv("OAUTH_TOKEN")
+    token=os.getenv("TOKEN")
     token_secret=os.getenv("TOKEN_SECRET")
-    if not api_key or not oauth_token:
+    if not api_key or not token:
         raise NotImplemented
 
     return TrelloClient(
         api_key=api_key,
         api_secret=api_secret,
-        token=oauth_token,
+        token=token,
         token_secret=token_secret
     )

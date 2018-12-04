3a62390565334a1f63699c2e7119b86dee614408
natasharamburrun <r_natasha100@hotmail.com>
2018-12-04 10:28:36 +0000

    added babelrc and react to app

--- app/src/app.js
+++ app/src/app.js
@@ -1,22 +1,36 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+// import 'bulma';
+import './static/style.scss';
 
 class App extends React.Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <h1>Test</h1>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+
+
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Kano Academy Dashboard</h1>
+      </main>
+
+
+
+    );
+  }
 }
-
-ReactDOM .render(
-    <App />,
-    document.getElementById('root')
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
---
+++ app/src/components/board/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
+class Navbar extends React.Component {
+
+}
+
+render() {
+    return (
+
+    );
+}
+
+export default withRouter(Navbar);
